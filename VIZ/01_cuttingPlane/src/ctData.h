c79e2efe4264518f77433dc61223fec8fcf3bd4b
kucera.ad <kucera.ad@6ea31c93-9679-6fd9-a685-619f95b28a77>
2011-03-13 15:54:37 +0000

    oprava chyby?

--- VIZ/01_cuttingPlane/src/ctData.h
+++ VIZ/01_cuttingPlane/src/ctData.h
@@ -18,8 +18,39 @@ public:
 	bool	loadFromFiles(const char * filename, int cnt, int scaleX, int scaleY, int scaleZ);
 
 	// get value at real position
-	float	getValueAt(const float x, const float y, const float z);
-	float	getValueAt(const v3 position);
+	inline float	getValueAt(const float x, const float y, const float z){
+		// get nearby coordinates
+		int xc, yc, zc;
+		float xt, yt,zt;
+		float val1, val2, val3, val4, val5, val6, val7, val8;
+		getCoordsForInterpolation(x, int(scX), xc, xt);
+		getCoordsForInterpolation(y, int(scY), yc, yt);
+		getCoordsForInterpolation(z, int(scZ), zc, zt); 
+
+		// get values
+		bool in = false;
+		val1 = getValueAt(xc  , yc  , zc  , in);
+		val2 = getValueAt(xc  , yc+1, zc  , in);
+		val3 = getValueAt(xc  , yc  , zc+1, in);
+		val4 = getValueAt(xc  , yc+1, zc+1, in);
+		val5 = getValueAt(xc+1, yc  , zc+1, in);
+		val6 = getValueAt(xc+1, yc+1, zc+1, in);
+		val7 = getValueAt(xc+1, yc  , zc  , in);
+		val8 = getValueAt(xc+1, yc+1, zc  , in);
+		float val = 0.0f;
+		if (in){
+			// at least one point is inside 3D grid... 
+			// interpolate then
+			val = interpolate3Dlin(val1, val2, val3, val4, val5, val6, val7, val8, xt, yt, zt);
+		} else {
+			return BACKGROUND_VALUE;
+		}
+
+		return val/3272;
+	}
+	inline float	getValueAt(const v3 position){
+		return getValueAt(position.x, position.y, position.z);
+	}
 
 	v3		getCenterPoint();
 	
@@ -31,7 +62,14 @@ private:
 	float max;
 
 	// get value at position in grid [not scaled]
-	float getValueAt(int x, int y, int z, bool &inFlag);
+	inline float getValueAt(int x, int y, int z, bool &inFlag){
+		if (x<0 || y<0 || z<0 || x>=dimX || y>=dimY || z>=dimZ){
+			// out of grid
+			return 0.0f;
+		}
+		inFlag = true;
+		return data[ z*dimX*dimY + y*dimX + x ];
+	}
 };
 
 
