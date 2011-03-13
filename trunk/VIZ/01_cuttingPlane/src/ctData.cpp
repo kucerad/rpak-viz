3bbc8257452f570dcb2a37fbb85a09fabe7404e9
kucera.ad <kucera.ad@6ea31c93-9679-6fd9-a685-619f95b28a77>
2011-03-13 15:54:37 +0000

    oprava chyby?

--- trunk/VIZ/01_cuttingPlane/src/ctData.cpp
+++ trunk/VIZ/01_cuttingPlane/src/ctData.cpp
@@ -11,7 +11,7 @@ CTdata::~CTdata(){
 }
 
 v3 CTdata::getCenterPoint(){
-	return v3(float(dimX)/2, float(dimY)/2,float(dimZ)/2);
+	return v3(scX*float(dimX)/2, scY*float(dimY)/2,scZ*float(dimZ)/2);
 }
 
 bool CTdata::loadFromFiles(const char * filename, int cnt, int scaleX, int scaleY, int scaleZ){
@@ -57,49 +57,4 @@ bool CTdata::loadFromFiles(const char * filename, int cnt, int scaleX, int scale
 	BACKSPACE(chars);
 	printf("LOADING CT images (%i) DONE\n", cnt);
 	return true;
-}
-
-
-float CTdata::getValueAt(int x, int y, int z, bool &inFlag){
-	if (x<0 || y<0 || z<0 || x>dimX || y>dimY || z>dimZ){
-		// out of grid
-		return 0.0f;
-	}
-	inFlag = true;
-	return data[ z*dimX*dimY + y*dimX + x ];
-}
-
-float CTdata::getValueAt(const v3 position){
-	return getValueAt(position.x, position.y, position.z);
-}
-
-float CTdata::getValueAt(const float x, const float y, const float z){
-	// get nearby coordinates
-	int xc, yc, zc;
-	float xt, yt,zt;
-	float val1, val2, val3, val4, val5, val6, val7, val8;
-	getCoordsForInterpolation(x, int(scX), xc, xt);
-	getCoordsForInterpolation(y, int(scY), yc, yt);
-	getCoordsForInterpolation(z, int(scZ), zc, zt); 
-
-	// get values
-	bool in = false;
-	val1 = getValueAt(xc  , yc  , zc  , in);
-	val2 = getValueAt(xc  , yc+1, zc  , in);
-	val3 = getValueAt(xc  , yc  , zc+1, in);
-	val4 = getValueAt(xc  , yc+1, zc+1, in);
-	val5 = getValueAt(xc+1, yc  , zc+1, in);
-	val6 = getValueAt(xc+1, yc+1, zc+1, in);
-	val7 = getValueAt(xc+1, yc  , zc  , in);
-	val8 = getValueAt(xc+1, yc+1, zc  , in);
-	float val = 0.0f;
-	if (in){
-		// at least one point is inside 3D grid... 
-		// interpolate then
-		val = interpolate3Dlin(val1, val2, val3, val4, val5, val6, val7, val8, xt, yt, zt);
-	} else {
-		return BACKGROUND_VALUE;
-	}
-
-	return val/3272;
 }
